3858b39fc8a643dbba8c80a5ebac07cf82e3bc03
Lucas Neves <lucas.ccdr@gmail.com>
2024-09-09 17:53:22 -0300

    Ajuste para redução de código.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
 public class Main {
     static int indice = 13;
     static int soma = 0;
-    static int k = 0;
 
     public static void main(String[] args) {
-
-        while (k < indice) {
-            k += 1;
+        for (int k = 0; k < indice; k++){
             soma += k;
             System.out.println(soma);
         }
-
         System.out.println("O valor da variável SOMA é " + soma);
-
     }
 }
